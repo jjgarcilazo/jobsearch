18be537b5061814baae15f8ae73c0c687086ce4b
jjgarcilazo <jjgarcilazo@gmail.com>
2020-05-27 17:16:35 -0700

    all files changed git push

---
+++ enviorment.rb
@@ -0,0 +1,11 @@
+#require all gems that is used 
+# require all files being used
+
+require 'net/http'
+require 'json'
+
+
+require_relative "./lib/api"
+require_relative "./lib/cli"
+require_relative "./lib/jobs"
+
