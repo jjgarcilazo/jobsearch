cebb3686290b5f0ae23408c5665516a1d489e6fc
Jjgarcilazo <jjgarcilazo@gmail.com>
2021-04-28 17:41:14 -0700

    refactored cli file

--- trabajo/Lib/cli.rb
+++ trabajo/Lib/cli.rb
@@ -4,9 +4,7 @@ class CLI
         puts "Welcome To Github Job search CLI"
         API.jobs
         self.menu
-          
-
-        # ^  start of the program in list order
+           # ^  start of the program in list order
     end
            # v here we build our menu and user inputs if any
            # along with responses ; in this case we ask what type of jobs to search for
@@ -15,31 +13,25 @@ class CLI
         user_input = gets.strip.downcase
         if user_input == "yes" || user_input == "y"
             puts "please wait while we search database for available jobs"
-            list_jobs
+            Jobs.all.each.with_index(1) do |job_listing, index| 
+                puts "#{index} for #{job_listing.title}"
+            end
             quest
-        
         else 
             contract_jobs    
-            
         end
     end
         
-    
-          
-                       
-                    # Here we add what responses to display in order. 
+             # Here we add what responses to display in order. 
         def quest
             #ask user for choice
             puts "Enter job listing number for more details "
-            
             index = gets.strip.to_i - 1
-            
             #validate input
             max_limit = Jobs.all.length - 1 
             until index.between?(0,max_limit) #if not valid
                 puts "sorry enter a valid number"
-                quest
-
+                quest     #our  looper
             end
               bun = Jobs.all[index]
                 #found thier choice
@@ -48,13 +40,39 @@ class CLI
                 
             
         end
-
+        def pt=(pt)
+            @pt = pt
+        end
+        def pt
+            @pt
+        end
         def contract_jobs                                                                                                                                                      
          puts "what about contract jobs?"
          user_input = gets.strip.downcase
           if user_input == "yes" || user_input == "y"
           puts "please wait while we locate availible contract jobs."
-          display_contract
+          @pt = Jobs.all.find { |work| work.type == 'contract' }
+        if pt.nil? 
+         puts "no contract jobs available at this time sorry"
+        
+        else
+          puts pt.title
+          puts pt.company
+          puts "Do you want more details ? "
+        user_input = gets.strip.downcase
+         if user_input == "yes" || user_input == "y"
+         puts " Hold on you ask for too much..."
+         detail_ft
+         @pt = Jobs.all.find { |detail| detail.type == "Contract" }
+           details
+        
+        exos
+
+       
+         end
+
+        end
+        cali
           menu
            else
             exos
@@ -80,39 +98,17 @@ class CLI
         def detailed_jobs(bun)
          # our response for full time jobs is built here 
          #it displays the jobs with certain info 
-         puts bun.title
-         puts bun.company
-         puts bun.location
-         puts bun.type
-          puts bun.url
-         cali   
+            puts bun.title
+                puts bun.company
+                puts bun.location
+                puts bun.type
+                puts bun.url
+            cali   
         end
        
 
     # end
-    def list_jobs
-        Jobs.all.each.with_index(1) do |job_listing, index| 
-            puts "#{index} for #{job_listing.title}"
-        end
-    end
-  
-
-    def display_contract
-       pt = Jobs.all.find { |work| work.type == 'contract' }
-        if pt.nil? 
-         puts "no contract jobs available at this time sorry"
-        
-        else
-          puts pt.title
-          puts pt.company
-          detail_pt
-
-        end
-        cali
-         # this is the response displayed if contract jobs are being searched
-            
-    end
-    def cali
+   def cali
         puts "Thanks For using Job Search CLI "
         puts " do you want to keep using CLI ?"
         user_input = gets.strip.downcase
@@ -125,29 +121,18 @@ class CLI
     end
      
     def detail_ft
-       Jobs.all.each do |detail|
-            puts detail.title
-            puts detail.company
-            puts detail.location
-            puts detail.type
-            puts detail.url
-            
-            
+       Jobs.all.each do |bun|
         end 
     end
 
-    def detail_pt 
+     def detail_pt 
         puts "Do you want more details ? "
         user_input = gets.strip.downcase
          if user_input == "yes" || user_input == "y"
          puts " Hold on you ask for too much..."
          detail_ft
-        detail = Jobs.all.find { |detail| detail.type == "Contract" }
-         puts detail.title
-            puts detail.company
-            puts detail.location
-            puts detail.type
-            puts detail.url
+         @pt = Jobs.all.find { |detail| detail.type == "Contract" }
+         details
         
         exos
 
